5c41661a80bac864df7b63f7146421a77446dac4
Tasibaev Davron <tdav@mail.ru>
2022-11-03 15:38:12 +0500

    11

--- Localization/Form1.cs
+++ Localization/Form1.cs
@@ -17,6 +17,7 @@ namespace Localization
 
         private void simpleButton1_Click(object sender, EventArgs e)
         {
+            //https://learn-automation.com/selenium-webdriver-c-sharp-tutorial/
 
             IWebDriver driver = new ChromeDriver();
 
