bbdf3bd55a4e7dd358bcd5ae67d7260687a78457
Sardor557 <turk557@mail.ru>
2022-11-04 15:05:59 +0500

    123

--- Localization/Form1.cs
+++ Localization/Form1.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium;
+﻿using Localization.Models;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -10,37 +12,43 @@ namespace Localization
 {
     public partial class Form1 : DevExpress.XtraEditors.XtraForm
     {
+        private readonly IWebDriver driver;
         public Form1()
         {
             InitializeComponent();
+            driver = new ChromeDriver("C:\\Works\\Devexpress.Localization");
         }
 
         private void simpleButton1_Click(object sender, EventArgs e)
         {
             //https://learn-automation.com/selenium-webdriver-c-sharp-tutorial/
 
-            IWebDriver driver = new ChromeDriver("C:\\Works\\Devexpress.Localization");
-
-            driver.Navigate().GoToUrl("https://localization.devexpress.com/Localization/Modify?version=2022.1&culture=uz-Cyrl-UZ");
-
             driver.Manage().Window.Maximize();
 
-            var diagnosticsTableContent = new List<Model>();
+            var diagnosticsTableContent = new TransModel();
 
             while (true)
             {
                 IWebElement diagnosticsTableBody = null;
+                IWebElement page = null;
                 try
                 {
                     diagnosticsTableBody = driver.FindElement(By.XPath("/html/body/main/div[2]/div[2]/div[1]/div[4]/table/tbody[1]"));
+                    page = driver.FindElement(By.XPath("/html/body/main/div[2]/div[2]/div[1]/div[5]"))
+                                 .FindElement(By.XPath("/html/body/main/div[2]/div[2]/div[1]/div[5]/div[2]"))
+                                 .FindElement(By.XPath("/html/body/main/div[2]/div[2]/div[1]/div[5]/div[2]/div"))
+                                 .FindElement(By.XPath("/html/body/main/div[2]/div[2]/div[1]/div[5]/div[2]/div/div"))
+                                 .FindElement(By.ClassName("pager__button--active"));
                 }
                 catch (Exception)
                 {
+                    int.TryParse(page.Text, out int curP);
+                    diagnosticsTableContent.CurrentPage = curP;
                     Task.Delay(15000).Wait();
                 }
 
-
                 int diagnosticsTableRowsCount = diagnosticsTableBody.FindElements(By.XPath("./tr")).Count;
+                diagnosticsTableContent.Models = new List<Model>();
 
                 for (int i = 0; i < diagnosticsTableRowsCount; i++)
                 {
@@ -52,7 +60,7 @@ namespace Localization
                     it.English = rowElements[1].Text;
                     it.Suggested = rowElements[2].Text;
 
-                    diagnosticsTableContent.Add(it);
+                    diagnosticsTableContent.Models.Add(it);
                 }
 
                 try
@@ -69,10 +77,55 @@ namespace Localization
 
 
         Tamom:
-            var txt = Newtonsoft.Json.JsonConvert.SerializeObject(diagnosticsTableContent, Newtonsoft.Json.Formatting.Indented);
+            var txt = JsonConvert.SerializeObject(diagnosticsTableContent, Formatting.Indented);
             File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "model.json", txt);
 
             driver.Close();
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "model.json");
+            var list = JsonConvert.DeserializeObject<TransModel>(json);
+            list.Models = list.Models.Where(x => x.Uz != null).ToList();
+
+            var translatorArea = driver.FindElement(By.XPath("/html/body/div[1]/main/div[1]"))
+                                       .FindElement(By.XPath("/html/body/div[1]/main/div[1]/div[1]"));
+
+            var clickToContinue = translatorArea.FindElement(By.XPath("/html/body/div[1]/main/div[1]/div[2]"));
+            clickToContinue.Click();
+
+            //*[@id="fakeArea"]
+            //*[@id="fakeArea"]
+            ///html/body/div[1]/main/div[1]/div[1]/div[3]/div[1]/div/div[1]/div[1]/div
+
+            var wordArea = translatorArea.FindElement(By.XPath("//*[@id=\"fakeArea\"]"));
+            wordArea.Clear();
+
+            var transWordArea = translatorArea.FindElement(By.XPath("//*[@id=\"translation\"]"));
+
+            foreach (var item in list.Models)
+            {
+                wordArea.SendKeys(item.English);
+                Task.Delay(1000).Wait();
+                item.Uz = transWordArea.Text;
+                wordArea.Clear();
+
+                json = JsonConvert.SerializeObject(list, Formatting.Indented);
+                File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "model.json", json);
+            }
+
+            driver.Close();
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().GoToUrl("https://translate.yandex.ru/?lang=en-uzbcyr");
+        }
+
+        private void simpleButton4_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().GoToUrl("https://localization.devexpress.com/Localization/Modify?version=2022.1&culture=uz-Cyrl-UZ");
         }
     }
 }
--- Localization/Models/Model.cs
+++ Localization/Models/Model.cs
@@ -1,10 +1,11 @@
-﻿namespace Localization
+﻿namespace Localization.Models
 {
     internal class Model
     {
         public string Name { get; set; }
         public string English { get; set; }
         public string Suggested { get; set; }
-        public string Translation { get; set; }
+        public string Uz { get; set; }
+        public string Lt { get; set; }
     }
 }
